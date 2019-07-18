a9188c35904f5b02b6e94c5688c65d7314b55df6
theschitz <theodore@theschitz.se>
2019-07-18 15:02:51 +0200

    Refactoring to class.

--- db.py
+++ db.py
@@ -1,110 +1,69 @@
 import configparser
 import mysql.connector
-from helpers import *
-
-config = configparser.ConfigParser()
-TEST_STRINGS = {"connection": ["[*] Testing database connection...",
-                               "[*] Server connection OK!"],
-                "database": ["[*] Checking if the database exists...",
-                             "[*] Database OK!"]
-                }
+from helpers import bcolors
 
 
-class Queries:
+class Database:
     INSERT_QUERY = "INSERT INTO " \
                    "readings(readingtimestamp, sensor, temperature, humidity, location) " \
                    "VALUES (%(timestamp)s, %(sensor)s, %(temp)s, %(humidity)s, %(location)s)"
     SELECT_QUERY = "SELECT * FROM readings WHERE {}"
 
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('database.cfg')
+        try:
+            self.cnx = mysql.connector.connect(host=self.config["database"]["host"], 
+                                    user=self.config["database"]["user"],
+                                    password=self.config["database"]["password"])
+        except mysql.connector.Error as err:
+            self.handle_error_code(err)
+            raise err
 
-def load_config():
-    config.read('DHT22.cfg')
+        self.cursor = self.cnx.cursor()
 
+    @staticmethod
+    def handle_error_code(error: mysql.connector.Error):
+            if error.errno == 2003:
+                print("Please check that you supplied the correct host.")
+            elif error.errno == 1045:
+                print(bcolors.WARNING, "Wrong username or password.", bcolors.ENDC)
+            elif error.errno == 1049:
+                print(bcolors.WARNING, "The database does not exist.", bcolors.ENDC)
+
+    def create_new_database(self, database_name):
+        try:
+            self.cursor.execute("CREATE DATABASE {}".format(database_name))
+        except mysql.connector.Error as err:
+            print(bcolors.FAIL, "[!] Could not create database", bcolors.ENDC)
+            print(bcolors.FAIL, "[!] Error message: ", err, bcolors.ENDC)
+
+        self.cnx.commit()
+
+    def table_exists(self) -> bool:
+        self.cursor.execute('SHOW TABLES')
+        for table in self.cursor:
+            return self.config["readwrite"]["table"] in table
+        return False
+
+    def create_table(self):
+        self.cursor.execute("CREATE TABLE {} (readingtimestamp datetime, sensor varchar(20), "
+                    "temperature decimal, humidity decimal, location varchar(20));".format(self.config["readwrite"]["table"]))
+        self.cnx.commit()
+
+    def describe_table(self, table_name):
+        self.cursor.execute("DESCRIBE {}".format(self.config["readwrite"]["table"]))
+        print(bcolors.UNDERLINE, "[ Table Columns ]", bcolors.ENDC)
+        for row in self.cursor:
+            print(row[0], ":", row[1])
+
+    def insert_row(self, data):
+        self.cursor.execute(Database.INSERT_QUERY, data)
+        self.cnx.commit()
 
 def get_reading_dict():
     read_dict = {"timestamp": "", "sensor": "", "temp": "", "humidity": "", "location": ""}
     return read_dict
 
-
-def create_new_database(database_name):
-    load_config()
-    cnx = mysql.connector.connect(host=config["database"]["host"],
-                                  user=config["database"]["user"],
-                                  password=config["database"]["password"])
-    cursor = cnx.cursor()
-    try:
-        cursor.execute("CREATE DATABASE {}".format(database_name))
-    except mysql.connector.Error as err:
-        print(bcolors.FAIL, "[!] Could not create database", bcolors.ENDC)
-        print(bcolors.FAIL, "[!] Error message: ", err, bcolors.ENDC)
-
-    cnx.commit()
-    cnx.close()
-
-
-def create_table():
-    load_config()
-    cnx = mysql.connector.connect(**dict(config.items('database')))
-    cursor = cnx.cursor()
-    cursor.execute("CREATE TABLE {} (readingtimestamp datetime, sensor varchar(20), "
-                   "temperature decimal, humidity decimal, location varchar(20));".format(config["readwrite"]["table"]))
-    cnx.commit()
-    cursor.execute("DESCRIBE {}".format(config["readwrite"]["table"]))
-    print(bcolors.UNDERLINE, "[ Table Columns ]", bcolors.ENDC)
-    for row in cursor:
-        print(row[0], ":", row[1])
-    cnx.close()
-
-
-def test_connection(cfg, test_for):
-    print(bcolors.OKBLUE, TEST_STRINGS[test_for][0], bcolors.ENDC)
-    if not len(cfg) > 0:
-        load_config()
-    try:
-        if len(cfg) > 0:
-            cnx = mysql.connector.connect(**cfg)
-        else:
-            cnx = mysql.connector.connect(**dict(config.items('database')))
-    except mysql.connector.Error as err:
-        print(bcolors.FAIL, err, bcolors.ENDC)
-        if err.errno == 2003:
-            print("Please check that you supplied the correct host.")
-        if err.errno == 1045:
-            print(bcolors.WARNING, "Wrong username or password.", bcolors.ENDC)
-        if err.errno == 1049:
-            print(bcolors.WARNING, "The database does not exist.", bcolors.ENDC)
-        return False
-    print(bcolors.OKGREEN, TEST_STRINGS[test_for][1], bcolors.ENDC)
-    cnx.close()
-    return True
-
-
-def table_exists():
-    load_config()
-    cnx = mysql.connector.connect(**dict(config.items('database')))
-    cursor = cnx.cursor()
-    cursor.execute('SHOW TABLES')
-    for table in cursor:
-        if config["readwrite"]["table"] in table:
-            return True
-    cnx.close()
-    return False
-
-
-def insert_row(data):
-    return run_query(Queries.INSERT_QUERY, data)
-
-
-def run_query(stmt, data=''):
-    load_config()
-    cnx = mysql.connector.connect(**dict(config.items('database')))
-    cursor = cnx.cursor()
-    cursor.execute(stmt, data)
-    cnx.commit()
-    cnx.close()
-    return True
-
-
 if __name__ == "__main__":
-    load_config()
-
+    db = Database()
