88c48f419e843677a4052b7dfdd6ae4fc6955860
theschitz <theodore@theschitz.se>
2019-07-18 16:00:43 +0200

    Refactoring started.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-import db
+from Database import Database
+from read_temp import TempReader
 from helpers import *
 import configparser
 from datetime import datetime, timedelta
@@ -7,18 +8,16 @@ import random
 
 def create_sample_data(no_of_rows):
     # readings(readingtimestamp, sensor, temperature, humidity, location)
+    db = Database()    
     for i in range(0, no_of_rows):
-        ts = datetime.now() - timedelta(days=i)
-        temp = random.randint(10, 30)
-        humidity = random.randint(30, 100)
         sensor = random.randint(1, 2)
         location = "Sample" + str(sensor)
-        reading = {"timestamp": ts,
-                   "sensor": sensor,
-                   "temp": temp,
-                   "humidity": humidity,
-                   "location": location}
-        db.insert_row(reading)
+        reader = TempReader(0, location)
+        reader.temperature = random.randint(10, 30)        
+        reader.humidity = random.randint(30, 100)
+        fake_reading = reader.get_dict()
+        fake_reading["timestamp"] = datetime.now() - timedelta(days=i)        
+        db.insert_row(fake_reading)
 
 
 def get_database_connection_details(cfg):
@@ -51,12 +50,14 @@ def create_config_file(cfg):
 
 def do_setup():
     cfg = {"database": {}, "readwrite": {}}
+    db = Database()
     print(bcolors.HEADER,
           "Hello! Seems like it's your first time running this program, let's do some setup...", bcolors.ENDC)
     get_database_connection_details(cfg)
-    db.test_connection(cfg["database"], "connection")
+    #db.test_connection(cfg["database"], "connection")
     get_database_and_table(cfg)
     create_config_file(cfg)
+    """
     if not db.test_connection(cfg["database"], "connection"):
         if query_yes_no("[!] The database \"{}\" doesn't exist. Create it?".format(cfg["database"]["database"])):
             db.create_new_database(cfg["database"]["database"])
@@ -67,7 +68,7 @@ def do_setup():
             print(bcolors.OKGREEN, "[*] Table Created.", bcolors.ENDC)
             if query_yes_no("Create sample data?"):
                 create_sample_data(input("How many rows?"))
-
+    """
 if __name__ == "__main__":
     do_setup()
     print(bcolors.HEADER, "Setup complete.", bcolors.ENDC)
