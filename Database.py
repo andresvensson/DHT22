b6c7691e1a146b199190041686f59a7050ae8fe7
theschitz <theodore@theschitz.se>
2019-07-22 16:20:44 +0200

    Removed colors. Added tests. Fixed bugs

--- Database.py
+++ Database.py
@@ -1,6 +1,6 @@
 import configparser
+from typing import Dict
 import mysql.connector
-from helpers import bcolors
 
 
 class Database:
@@ -16,25 +16,29 @@ class Database:
     def __init__(self):
         self.config = configparser.ConfigParser()
         self.config.read('database.cfg')
+        self.table_name: str = self.config['readwrite']['table']
+        self.self_db: str = self.config['readwrite']['database']
         try:
             self.cnx = mysql.connector.connect(
-                host=self.config["database"]["host"],
-                user=self.config["database"]["user"],
-                password=self.config["database"]["password"]
-                )
-        except mysql.connector.Error as err:
+                    host=self.config["database"]["host"],
+                    user=self.config["database"]["user"],
+                    password=self.config["database"]["password"]
+                    )
+            self.cursor = self.cnx.cursor()
+        except mysql.connector.error as err:
             self.handle_error_code(err)
-            raise err
+            raise(err)
 
-        self.cursor = self.cnx.cursor()
-
-    def create_new_database(self, database_name):
+    def create_new_database(self):
+        print(f"Creating database: {self.db_name}")
         try:
-            self.cursor.execute("CREATE DATABASE {}".format(database_name))
+            self.cursor.execute(f"CREATE DATABASE IF NOT EXISTS {self.db_name}")
+            self.cnx.commit()
         except mysql.connector.Error as err:
-            print(bcolors.FAIL, "[!] Could not create database", bcolors.ENDC)
-            raise err
-        self.cnx.commit()
+            print("[!] Could not create database")
+            self.handle_error_code(err)
+        finally:
+            self.cursor.execute(f"USE {self.db_name}")
 
     def table_exists(self) -> bool:
         self.cursor.execute('SHOW TABLES')
@@ -42,17 +46,23 @@ class Database:
             return self.config["readwrite"]["table"] in table
         return False
 
-    def create_table(self):
-        query = f"""CREATE TABLE {self.config['readwrite']['table']}
+    def set_use_db(self):
+        self.cursor.execute(f"USE {self.db_name}")
+
+    def create_table(self) -> None:
+        if self.table_exists():
+            return
+        self.set_use_db()
+        query = f"""CREATE TABLE {self.table_name}
                 (readingtimestamp datetime, sensor varchar(20),
                 temperature decimal, humidity decimal,
                 location varchar(20));"""
         self.cursor.execute(query)
         self.cnx.commit()
 
-    def describe_table(self, table_name):
-        self.cursor.execute(f"DESCRIBE {self.config['readwrite']["table"]}")
-        print(bcolors.UNDERLINE, "[ Table Columns ]", bcolors.ENDC)
+    def describe_table(self):
+        self.cursor.execute(f"DESCRIBE {self.table_name}")
+        print("[ Table Columns ]")
         for row in self.cursor:
             print(row[0], ":", row[1])
 
@@ -62,13 +72,14 @@ class Database:
 
     @staticmethod
     def handle_error_code(error: mysql.connector.Error):
-        error_codes = {
+        error_codes: Dict[int, str] = {
             2003: "Please check that you supplied the correct host.",
             1045: "Wrong username or password.",
             1049: "The database does not exist."
         }
-        if error_codes[error.errno]:
-            print(bcolors.WARNING, error_codes[error.errno], bcolors.ENDC)
+
+        if error_codes.get(error.errno):
+            print(error_codes[error.errno])
 
 
 if __name__ == "__main__":
