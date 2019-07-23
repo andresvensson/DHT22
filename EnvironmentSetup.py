a2673783b7218edd0da53cb0d256c9c4c94cbd52
theschitz <theodore@theschitz.se>
2019-07-23 17:42:55 +0200

    rebuilt as class ConfigCreator.

--- EnvironmentSetup.py
+++ EnvironmentSetup.py
@@ -1,75 +1,65 @@
 from Database import Database
 from TempReader import TempReader
-from helpers import bcolors, query_yes_no
+from helpers import query_yes_no
 import configparser
 from datetime import datetime, timedelta
 import random
 
 
-def create_sample_data(no_of_rows):
-    # readings(readingtimestamp, sensor, temperature, humidity, location)
-    db = Database()    
-    for i in range(0, no_of_rows):
-        sensor = random.randint(1, 2)
-        location = "Sample" + str(sensor)
-        reader = TempReader(0, location)
-        reader.temperature = random.randint(10, 30)        
-        reader.humidity = random.randint(30, 100)
-        fake_reading = reader.get_dict()
-        fake_reading["timestamp"] = datetime.now() - timedelta(days=i)        
-        db.insert_row(fake_reading)
+class ConfigCreator:
+    def __init__(self):
+        self.config: Dict = {}
+        self.config_file_name: str = ""
+
+    def create_section(self, key: str, values: list[str]):
+        print(f'[{key}]')
+        for v in values:
+            self.config[key] = input(f'{v}: ')
+
+    def create_config_file(self):
+        conf = configparser.ConfigParser()
+        for section in cfg:
+            conf.add_section(section)
+            for key, value in cfg[section]:
+                conf.set(section, key, value)
+        self.config_file_name = input('Set config file name: ')
+        with open(self.config_file_name, 'w') as file:
+            conf.write(file)
+            print("Config file created: ", file.name)
+
+    def print_config(self):
+        print("-" * 20)
+        print("Config")
+        print("-" * 20)
+        for section in self.config as cfg:
+            print(f"[{section}]")
+            for key, value in cfg[section]:
+                print(key, ':', value)
+        print("-" * 20)
 
 
-def get_database_connection_details(cfg):
-    cfg["database"]["host"] = input("Database host: ")
-    cfg["database"]["user"] = input("Database user: ")
-    cfg["database"]["password"] = input("Password: ")
-
-
-def get_database_and_table(cfg):
-    cfg["database"]["database"] = input("Database name: ")
-    cfg["readwrite"]["table"] = input("Database table name: ")
-
-
-def create_config_file(cfg):
-    conf = configparser.ConfigParser()
-    print("-"*20)
-    print("Your config: ")
-    print("-" * 20)
-    for key in cfg:
-        conf.add_section(key)
-        for key2 in cfg[key]:
-            print(key2, ':', cfg[key][key2])
-            conf.set(key, key2, cfg[key][key2])
-    print("-" * 20)
-    with open('DHT22.cfg', 'w') as file:
-        conf.write(file)
-        print("Config file created: ", file.name)
-    print("-" * 20)
-
-
-def do_setup():
-    cfg = {"database": {}, "readwrite": {}}
-    db = Database()
+def create_config():
+    cfg = ConfigCreator()
     print("""Hello! Seems like it's your first time running this program,
             let's do some setup...
         """)
-    get_database_connection_details(cfg)
-    #db.test_connection(cfg["database"], "connection")
-    get_database_and_table(cfg)
-    create_config_file(cfg)
-    """
-    if not db.test_connection(cfg["database"], "connection"):
-        if query_yes_no("[!] The database \"{}\" doesn't exist. Create it?".format(cfg["database"]["database"])):
+    cfg.create_section('database', ['host', 'user', 'password'])
+    cfg.create_section('readwrite', ['database', 'table'])
+    cfg.print_config()
+    cfg.create_config_file()
+    db = Database(cfg.config_file_name)
+    if not db.connect():
+        qst = f"[!] Create database \"{cfg['database']['database']}\"?"
+        if query_yes_no(qst):
             db.create_new_database(cfg["database"]["database"])
-            print(bcolors.OKGREEN, "[*] Database Created.", bcolors.ENDC)
+            print("[*] Database Created.")
     if not db.table_exists():
         if query_yes_no("[!] The table {} doesn't exist. Create it?".format(cfg["readwrite"]["table"])):
             db.create_table()
-            print(bcolors.OKGREEN, "[*] Table Created.", bcolors.ENDC)
+            print("[*] Table Created.")
             if query_yes_no("Create sample data?"):
                 create_sample_data(input("How many rows?"))
-    """
+
 
 if __name__ == "__main__":
     do_setup()
